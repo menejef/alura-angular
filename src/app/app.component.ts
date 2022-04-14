2c11050468a4f786e0c04de3074a935d8d628831
Jefferson de Menezes Tupinambá <74921336+menejef@users.noreply.github.com>
2022-04-13 23:21:50 -0300

    Criando o primeiro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+//Propriedades
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+//Classe
 export class AppComponent {
   title = 'bytebank';
 }
